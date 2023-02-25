8434698db52571f7eb36c49e5f50596ac2a2cf06
Joel Hagan <joel.hagan.8@gmail.com>
2023-02-24 18:27:15 -0700

    update query for adding new listing

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -177,7 +177,6 @@ const getAllProperties = function(options, limit = 10) {
     .catch((err) => {
       console.log(err.message);
     });
-
 };
 exports.getAllProperties = getAllProperties;
 
@@ -188,9 +187,21 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let queryParams = [];
+
+  for (let key in property) {
+    queryParams.push(property[key]);
+  }
+
+  return pool
+    .query(`
+    INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `, queryParams)
+    .then(result => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addProperty = addProperty;
